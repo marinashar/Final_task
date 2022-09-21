e88104e7bee437e6d12637f88c73227697398d7c
Marina <marinosharipo@gmail.com>
2022-09-21 16:12:50 +0700

    added task

---
+++ task/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.Write("Ведите число элементов: ");
+int size = int.Parse(Console.ReadLine());
+
+string[] Array = new string[size];
+for (int p = 0; p < size; p++)
+{
+    Console.WriteLine($"Введите {p+1} элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    Array[p] = element;
+}
+
+string[] FinalArray = new string[size];
+int i = 3;
+int j = 0;
+
+for (int k = 0; k < size; k++)
+{
+    if (Array[k].Length <= i)
+    {
+        FinalArray[j] = Array[k];
+        j++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(FinalArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
